d6489071c341c1961224042f62b008b3ebe28c09
Kosorukov Anatoly <java1cprog@yandex.ru>
2018-02-15 06:36:26 +0200

    Setup composer settings

---
+++ app/start.php
@@ -0,0 +1,12 @@
+<?php
+
+session_cache_limiter(false);
+session_start();
+
+ini_set('display_errors', 'On');
+
+define('INC_ROOT', dirname(__DIR__));
+
+require INC_ROOT . '/vendor/autoload.php';
+
+$app = new Slim();
