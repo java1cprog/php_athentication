d41a66064c491e4fedca1452e8731f53f4f16f4f
Kosorukov Anatoly <java1cprog@yandex.ru>
2018-02-21 14:41:17 +0200

    add something new

---
+++ app/config/production.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+  'app' => [
+    'url' => 'http://localhost',
+    'hash' => [
+      'algo' => PASSWORD_BCRYPT,
+      'cost' => 10
+    ]
+  ],
+  'db' => [
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'name' => 'site',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => ''
+  ],
+  'auth' => [
+    'session' => 'user_id',
+    'remember' => 'user_r'
+  ],
+  'mail' => [
+    'smtp_auth' => true,
+    'smtp_secure' => 'tls',
+    'host' => 'smtp.gmail.com',
+    'username' => 'java1cprog@gmail.com',
+    'password' => 'kas1234kas1234',
+    'port' => 587,
+    'html' => true,
+  ],
+
+  'twig' => [
+    'debug' => true
+  ],
+
+  'twig' => [
+    'debug' => true
+  ],
+
+  'csrf' => [
+    'session' => 'csrf_token'
+  ]
+];
--- app/start.php
+++ app/start.php
@@ -11,8 +11,15 @@ define('INC_ROOT', dirname(__DIR__));
 
 require INC_ROOT . '/vendor/autoload.php';
 
-$app = new App();
+$app = new App([
+  'mode' => file_get_contents(INC_ROOT . '/mode.php')
+]);
 
-$app->get('/test/{name}', function ($request, $response, $args) {
-    return $response->getBody()->write('Hello, ' . $args['name']);
-});
+$app->configureMode($config->get('mode'), function() use ($app) {
+   $app->config = Config::load(INC_ROOT . "/app/config/{$app->mode}"); });
+echo $app->config->get('db.driver');
+//echo $app->config('mode');
+
+//$app->get('/test/{name}', function ($request, $response, $args) {
+//    return $response->getBody()->write('Hello, ' . $args['name']);
+//});
